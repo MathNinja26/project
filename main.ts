2c69ba7537930eb5735fafaf6acb559c2c4ba88a
MathNinja26 <Estherdentel@gmail.com>
2025-02-27 10:04:41 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,158 +1,214 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    x = mySprite.x
-    y = mySprite.y
-    sprites.destroy(mySprite)
-    mySprite = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . f f f f . . . . . . 
-        . . . . f f e e e e f f . . . . 
-        . . . f e e e f f e e e f . . . 
-        . . . f f f f 2 2 f f f f . . . 
-        . . f f e 2 e 2 2 e 2 e f f . . 
-        . . f e 2 f 2 f f f 2 f e f . . 
-        . . f f f 2 f e e 2 2 f f f . . 
-        . . f e 2 f f e e 2 f e e f . . 
-        . f f e f f e e e f e e e f f . 
-        . f f e e e e e e e e e e f f . 
-        . . . f e e e e e e e e f . . . 
-        . . . e f f f f f f f f 4 e . . 
-        . . . 4 f 2 2 2 2 2 e d d 4 . . 
-        . . . e f f f f f f e e 4 . . . 
-        . . . . f f f . . . . . . . . . 
-        `, SpriteKind.Player)
-    controller.moveSprite(mySprite)
-    mySprite.x = x
-    mySprite.y = y
-    pause(100)
-    x = mySprite.x
-    y = mySprite.y
-    sprites.destroy(mySprite)
-    mySprite = sprites.create(img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f e e e e f f . . . . 
-        . . . f e e e f f e e e f . . . 
-        . . f f f f f 2 2 f f f f f . . 
-        . . f f e 2 e 2 2 e 2 e f f . . 
-        . . f e 2 f 2 f f 2 f 2 e f . . 
-        . . f f f 2 2 e e 2 2 f f f . . 
-        . f f e f 2 f e e f 2 f e f f . 
-        . f e e f f e e e e f e e e f . 
-        . . f e e e e e e e e e e f . . 
-        . . . f e e e e e e e e f . . . 
-        . . e 4 f f f f f f f f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
-        `, SpriteKind.Player)
-    controller.moveSprite(mySprite)
-    mySprite.x = x
-    mySprite.y = y
-    pause(100)
-    x = mySprite.x
-    y = mySprite.y
-    sprites.destroy(mySprite)
-    mySprite = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . f f f f . . . . . . 
-        . . . . f f e e e e f f . . . . 
-        . . . f e e e f f e e e f . . . 
-        . . . f f f f 2 2 f f f f . . . 
-        . . f f e 2 e 2 2 e 2 e f f . . 
-        . . f e f 2 f f f 2 f 2 e f . . 
-        . . f f f 2 2 e e f 2 f f f . . 
-        . . f e e f 2 e e f f 2 e f . . 
-        . f f e e e f e e e f f e f f . 
-        . f f e e e e e e e e e e f f . 
-        . . . f e e e e e e e e f . . . 
-        . . e 4 f f f f f f f f e . . . 
-        . . 4 d d e 2 2 2 2 2 f 4 . . . 
-        . . . 4 e e f f f f f f e . . . 
-        . . . . . . . . . f f f . . . . 
-        `, SpriteKind.Player)
-    controller.moveSprite(mySprite)
-    mySprite.x = x
-    mySprite.y = y
-    pause(100)
-    x = mySprite.x
-    y = mySprite.y
-    sprites.destroy(mySprite)
-    mySprite = sprites.create(img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f e e e e f f . . . . 
-        . . . f e e e f f e e e f . . . 
-        . . f f f f f 2 2 f f f f f . . 
-        . . f f e 2 e 2 2 e 2 e f f . . 
-        . . f e 2 f 2 f f 2 f 2 e f . . 
-        . . f f f 2 2 e e 2 2 f f f . . 
-        . f f e f 2 f e e f 2 f e f f . 
-        . f e e f f e e e e f e e e f . 
-        . . f e e e e e e e e e e f . . 
-        . . . f e e e e e e e e f . . . 
-        . . e 4 f f f f f f f f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
-        `, SpriteKind.Player)
-    controller.moveSprite(mySprite)
-    mySprite.x = x
-    mySprite.y = y
-    pause(100)
+    while (controller.up.isPressed()) {
+        x = mySprite.x
+        y = mySprite.y
+        sprites.destroy(mySprite)
+        mySprite = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . f f f f . . . . . . 
+            . . . . f f e e e e f f . . . . 
+            . . . f e e e f f e e e f . . . 
+            . . . f f f f 2 2 f f f f . . . 
+            . . f f e 2 e 2 2 e 2 e f f . . 
+            . . f e 2 f 2 f f f 2 f e f . . 
+            . . f f f 2 f e e 2 2 f f f . . 
+            . . f e 2 f f e e 2 f e e f . . 
+            . f f e f f e e e f e e e f f . 
+            . f f e e e e e e e e e e f f . 
+            . . . f e e e e e e e e f . . . 
+            . . . e f f f f f f f f 4 e . . 
+            . . . 4 f 2 2 2 2 2 e d d 4 . . 
+            . . . e f f f f f f e e 4 . . . 
+            . . . . f f f . . . . . . . . . 
+            `, SpriteKind.Player)
+        controller.moveSprite(mySprite)
+        mySprite.x = x
+        mySprite.y = y
+        pause(100)
+        x = mySprite.x
+        y = mySprite.y
+        sprites.destroy(mySprite)
+        mySprite = sprites.create(img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f e e e e f f . . . . 
+            . . . f e e e f f e e e f . . . 
+            . . f f f f f 2 2 f f f f f . . 
+            . . f f e 2 e 2 2 e 2 e f f . . 
+            . . f e 2 f 2 f f 2 f 2 e f . . 
+            . . f f f 2 2 e e 2 2 f f f . . 
+            . f f e f 2 f e e f 2 f e f f . 
+            . f e e f f e e e e f e e e f . 
+            . . f e e e e e e e e e e f . . 
+            . . . f e e e e e e e e f . . . 
+            . . e 4 f f f f f f f f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `, SpriteKind.Player)
+        controller.moveSprite(mySprite)
+        mySprite.x = x
+        mySprite.y = y
+        pause(100)
+        x = mySprite.x
+        y = mySprite.y
+        sprites.destroy(mySprite)
+        mySprite = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . f f f f . . . . . . 
+            . . . . f f e e e e f f . . . . 
+            . . . f e e e f f e e e f . . . 
+            . . . f f f f 2 2 f f f f . . . 
+            . . f f e 2 e 2 2 e 2 e f f . . 
+            . . f e f 2 f f f 2 f 2 e f . . 
+            . . f f f 2 2 e e f 2 f f f . . 
+            . . f e e f 2 e e f f 2 e f . . 
+            . f f e e e f e e e f f e f f . 
+            . f f e e e e e e e e e e f f . 
+            . . . f e e e e e e e e f . . . 
+            . . e 4 f f f f f f f f e . . . 
+            . . 4 d d e 2 2 2 2 2 f 4 . . . 
+            . . . 4 e e f f f f f f e . . . 
+            . . . . . . . . . f f f . . . . 
+            `, SpriteKind.Player)
+        controller.moveSprite(mySprite)
+        mySprite.x = x
+        mySprite.y = y
+        pause(100)
+        x = mySprite.x
+        y = mySprite.y
+        sprites.destroy(mySprite)
+        mySprite = sprites.create(img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f e e e e f f . . . . 
+            . . . f e e e f f e e e f . . . 
+            . . f f f f f 2 2 f f f f f . . 
+            . . f f e 2 e 2 2 e 2 e f f . . 
+            . . f e 2 f 2 f f 2 f 2 e f . . 
+            . . f f f 2 2 e e 2 2 f f f . . 
+            . f f e f 2 f e e f 2 f e f f . 
+            . f e e f f e e e e f e e e f . 
+            . . f e e e e e e e e e e f . . 
+            . . . f e e e e e e e e f . . . 
+            . . e 4 f f f f f f f f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `, SpriteKind.Player)
+        controller.moveSprite(mySprite)
+        mySprite.x = x
+        mySprite.y = y
+        pause(100)
+    }
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    while (controller.left.isPressed()) {
+        x = mySprite.x
+        y = mySprite.y
+        sprites.destroy(mySprite)
+        mySprite = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . f f f f f f . . . . . . 
+            . . . f 2 f e e e e f f . . . . 
+            . . f 2 2 2 f e e e e f f . . . 
+            . . f e e e e f f e e e f . . . 
+            . f e 2 2 2 2 e e f f f f . . . 
+            . f 2 e f f f f 2 2 2 e f . . . 
+            . f f f e e e f f f f f f f . . 
+            . f e e 4 4 f b e 4 4 e f f . . 
+            . . f e d d f 1 4 d 4 e e f . . 
+            . . . f d d d e e e e e f . . . 
+            . . . f e 4 e d d 4 f . . . . . 
+            . . . f 2 2 e d d e f . . . . . 
+            . . f f 5 5 f e e f f f . . . . 
+            . . f f f f f f f f f f . . . . 
+            . . . f f f . . . f f . . . . . 
+            `, SpriteKind.Player)
+        mySprite.x = x
+        mySprite.y = y
+        controller.moveSprite(mySprite)
+        pause(100)
+        x = mySprite.x
+        y = mySprite.y
+        sprites.destroy(mySprite)
+        mySprite = sprites.create(img`
+            . . . . f f f f f f . . . . . . 
+            . . . f 2 f e e e e f f . . . . 
+            . . f 2 2 2 f e e e e f f . . . 
+            . . f e e e e f f e e e f . . . 
+            . f e 2 2 2 2 e e f f f f . . . 
+            . f 2 e f f f f 2 2 2 e f . . . 
+            . f f f e e e f f f f f f f . . 
+            . f e e 4 4 f b e 4 4 e f f . . 
+            . . f e d d f 1 4 d 4 e e f . . 
+            . . . f d d d d 4 e e e f . . . 
+            . . . f e 4 4 4 e e f f . . . . 
+            . . . f 2 2 2 e d d 4 . . . . . 
+            . . . f 2 2 2 e d d e . . . . . 
+            . . . f 5 5 4 f e e f . . . . . 
+            . . . . f f f f f f . . . . . . 
+            . . . . . . f f f . . . . . . . 
+            `, SpriteKind.Player)
+        mySprite.x = x
+        mySprite.y = y
+        controller.moveSprite(mySprite)
+        pause(100)
+    }
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    x = mySprite.x
-    y = mySprite.y
-    sprites.destroy(mySprite)
-    mySprite = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . f f f f f f . . . . . 
-        . . . f f e e e e f 2 f . . . . 
-        . . f f e e e e f 2 2 2 f . . . 
-        . . f e e e f f e e e e f . . . 
-        . . f f f f e e 2 2 2 2 e f . . 
-        . . f e 2 2 2 f f f f e 2 f . . 
-        . f f f f f f f e e e f f f . . 
-        . f f e 4 4 e b f 4 4 e e f . . 
-        . f e e 4 d 4 1 f d d e f . . . 
-        . . f e e e e e d d d f . . . . 
-        . . . . f 4 d d e 4 e f . . . . 
-        . . . . f e d d e 2 2 f . . . . 
-        . . . f f f e e f 5 5 f f . . . 
-        . . . f f f f f f f f f f . . . 
-        . . . . f f . . . f f f . . . . 
-        `, SpriteKind.Player)
-    mySprite.x = x
-    mySprite.y = y
-    controller.moveSprite(mySprite)
-    pause(100)
-    x = mySprite.x
-    y = mySprite.y
-    sprites.destroy(mySprite)
-    mySprite = sprites.create(img`
-        . . . . . f f f f f f . . . . . 
-        . . . f f e e e e f 2 f . . . . 
-        . . f f e e e e f 2 2 2 f . . . 
-        . . f e e e f f e e e e f . . . 
-        . . f f f f e e 2 2 2 2 e f . . 
-        . . f e 2 2 2 f f f f e 2 f . . 
-        . f f f f f f f e e e f f f . . 
-        . f f e 4 4 e b f 4 4 e e f . . 
-        . f e e 4 d 4 1 f d d e f f . . 
-        . . f e e e 4 d d d d f d d f . 
-        . . . f f e e 4 e e e f b b f . 
-        . . . . f 2 2 2 4 d d e b b f . 
-        . . . . e 2 2 2 e d d e b f . . 
-        . . . . f 4 4 4 f e e f f . . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . . f f f . . . . . . . 
-        `, SpriteKind.Player)
-    mySprite.x = x
-    mySprite.y = y
-    controller.moveSprite(mySprite)
+    while (controller.right.isPressed()) {
+        x = mySprite.x
+        y = mySprite.y
+        sprites.destroy(mySprite)
+        mySprite = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . f f f f f f . . . . . 
+            . . . f f e e e e f 2 f . . . . 
+            . . f f e e e e f 2 2 2 f . . . 
+            . . f e e e f f e e e e f . . . 
+            . . f f f f e e 2 2 2 2 e f . . 
+            . . f e 2 2 2 f f f f e 2 f . . 
+            . f f f f f f f e e e f f f . . 
+            . f f e 4 4 e b f 4 4 e e f . . 
+            . f e e 4 d 4 1 f d d e f . . . 
+            . . f e e e e e d d d f . . . . 
+            . . . . f 4 d d e 4 e f . . . . 
+            . . . . f e d d e 2 2 f . . . . 
+            . . . f f f e e f 5 5 f f . . . 
+            . . . f f f f f f f f f f . . . 
+            . . . . f f . . . f f f . . . . 
+            `, SpriteKind.Player)
+        mySprite.x = x
+        mySprite.y = y
+        controller.moveSprite(mySprite)
+        pause(100)
+        x = mySprite.x
+        y = mySprite.y
+        sprites.destroy(mySprite)
+        mySprite = sprites.create(img`
+            . . . . . f f f f f f . . . . . 
+            . . . f f e e e e f 2 f . . . . 
+            . . f f e e e e f 2 2 2 f . . . 
+            . . f e e e f f e e e e f . . . 
+            . . f f f f e e 2 2 2 2 e f . . 
+            . . f e 2 2 2 f f f f e 2 f . . 
+            . f f f f f f f e e e f f f . . 
+            . f f e 4 4 e b f 4 4 e e f . . 
+            . f e e 4 d 4 1 f d d e f f . . 
+            . . f e e e 4 d d d d f d d f . 
+            . . . f f e e 4 e e e f b b f . 
+            . . . . f 2 2 2 4 d d e b b f . 
+            . . . . e 2 2 2 e d d e b f . . 
+            . . . . f 4 4 4 f e e f f . . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . . f f f . . . . . . . 
+            `, SpriteKind.Player)
+        mySprite.x = x
+        mySprite.y = y
+        controller.moveSprite(mySprite)
+        pause(100)
+    }
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 	
