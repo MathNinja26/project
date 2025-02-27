a1bb99519d3482077c2632f2c6fe6842f7f499ec
MathNinja26 <Estherdentel@gmail.com>
2025-02-27 09:12:12 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+let mySprite: Sprite = null
+tiles.setTileAt(tiles.getTileLocation(0, 0), assets.tile`transparency16`)
+controller.moveSprite(mySprite)
