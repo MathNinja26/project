184fbe6ca5d90903f6d756cd4e3e38d21d4e2a13
MathNinja26 <Estherdentel@gmail.com>
2025-02-28 19:53:19 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -364,8 +364,40 @@ scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
     sprites.destroy(sprite)
 })
 function Level4 () {
+    sprites.destroy(mySprite)
+    mySprite = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
     tiles.setCurrentTilemap(tilemap`level8`)
     tiles.placeOnTile(mySprite, tiles.getTileLocation(7, 7))
+    for (let index = 0; index < 20; index++) {
+        coins = sprites.create(img`
+            . . . b b . . . 
+            . . b 5 5 b . . 
+            . b 5 d 1 5 b . 
+            . b 5 3 1 5 b . 
+            . c 5 3 1 d c . 
+            . c 5 1 d d c . 
+            . . f d d f . . 
+            . . . f f . . . 
+            `, SpriteKind.Coin)
+        tiles.placeOnRandomTile(coins, sprites.builtin.coral1)
+    }
 }
 sprites.onCreated(SpriteKind.Player, function (sprite) {
     controller.moveSprite(sprite)
